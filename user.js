1c6eb1ed0402e0d62bf57f379ae6e553869c77ce
ericswenson0 <ericglennswenson@gmail.com>
2026-03-11 12:00:41 -0500

    Trying to get Aikido to pick up the illogical code

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 // Programmer assumes: user might be null or valid
-let name = user.getName();  // Assumes: user is not null
-let email = user.getEmail(); // Assumes: user is not null
+var name = user.getName();  // Assumes: user is not null
+var email = user.getEmail(); // Assumes: user is not null
 
 if (user == null) {  // Assumes: user might be null
     return defaultUser();  // But would have crashed above if true!
